f85c8c1607220676e509367df3ae05a5ec9278b0
yoshikouki <yoshikouki@pepabo.com>
2025-12-25 16:30:15 +0900

    Update URL

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,7 +1,6 @@
-export const EXTENSION_ID = "@@extension_id";
-
 export const urls = {
-  chromeWebStore: `https://chromewebstore.google.com/detail/flash-translate/${EXTENSION_ID}`,
+  chromeWebStore:
+    "https://chromewebstore.google.com/detail/flash-translate/gibfhkdnpbinchfllkhaihmghbbollfd",
   repository: "https://github.com/yoshikouki/flash-translate",
   support: "https://forms.gle/Kjbebqeu2Uqk7eX6A",
 } as const;
